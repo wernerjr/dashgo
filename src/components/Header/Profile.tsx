8744c7d0913c2ebe8e3558a3aa76b75149bf5c13
Werner Gehrke Junior <werner.junior@outlook.com>
2021-04-18 15:51:05 -0300

    responsividade header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-    <Box mr="4" textAlign="right">
-      <Text>Werner Junior</Text>
-      <Text color="gray.300" fontSize="small">
-        werner.junior@outlook.com
-      </Text>
-    </Box>
-
+      {showProfileData && (   
+      <Box mr="4" textAlign="right">
+        <Text>Werner Junior</Text>
+        <Text color="gray.300" fontSize="small">
+          werner.junior@outlook.com
+        </Text>
+      </Box>
+      )}
     <Avatar size="md" name="Werner Junior" src="https://github.com/wernerjr.png"/>
   </Flex>
   );
