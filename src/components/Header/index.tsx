2a67b8cccaa94b835636dbce08efd28953bc1cf9
Werner Gehrke Junior <werner.junior@outlook.com>
2021-04-18 16:16:33 -0300

    Sidebar responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../context/SidebarDrawerContext";
 import Logo from "./Logo";
 import NotificationNav from "./NotificationsNav";
 import { Profile } from "./Profile";
 import SearchBox from "./SearchBox";
 
 export default function Header(){
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -21,6 +25,16 @@ export default function Header(){
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton 
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine}/>}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        />
+      )}
       <Logo />
       {isWideVersion && <SearchBox />}      
       <Flex align="center" ml="auto">
