51bff5a108f582f0fcfa410d6d045351f5264f06
Werner Gehrke Junior <werner.junior@outlook.com>
2021-04-26 20:23:10 -0300

    Utilizando formulários com react

--- src/pages/Input.tsx
+++ src/pages/Input.tsx
@@ -1,14 +1,20 @@
 import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps } from '@chakra-ui/react';
+import { FieldErrors } from 'react-hook-form';
+import { forwardRef, ForwardRefRenderFunction } from 'react';
+import { FormErrorMessage } from '@chakra-ui/react';
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldErrors;
 }
 
-export function Input({name, label, ...rest}: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> 
+= ({name, label, error = null, ...rest}, ref) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
     { !!label && (<FormLabel htmlFor={name}>{label}</FormLabel>) }
+
     <ChakraInput
       name={name}
       id={name}
@@ -19,8 +25,16 @@ export function Input({name, label, ...rest}: InputProps) {
         bgColor:"gray.900"
       }}
       size="lg"
+      ref={ref}
       {...rest}
     />
+
+  {!!error && 
+    <FormErrorMessage>{error.message}</FormErrorMessage>
+  }
+    
   </FormControl>
   )
 }
+
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
 import { Flex, Button, Stack } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { Input } from './Input';
 
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória'),
+})
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+  
+  const { errors } = formState;
+
+  const  handleSignIn: SubmitHandler<SignInFormData> = (values) => {
+    console.log(values);
+  }
+
   return (
     <Flex
       w="100vw"
@@ -16,11 +39,24 @@ export default function SignIn() {
         bg="gray.800"
         p="8"
         borderRadius={8}
-        flexDir="column"
+        flexDir="column"  
+        onSubmit={handleSubmit(handleSignIn)}      
       >
         <Stack spacing={4}>
-          <Input name="email" label="E-mail" type="email" />
-          <Input name="password" label="Senha" type="password" />
+          <Input
+            name="email"
+            label="E-mail"
+            type="email"
+            error={errors.email}
+            {...register('email')}
+          />
+          <Input
+            name="password"
+            label="Senha"
+            type="password"
+            error={errors.password}
+            {...register('password')}
+          />
         </Stack>
 
         <Button
@@ -28,6 +64,7 @@ export default function SignIn() {
           mt="6"
           colorScheme="pink"
           size="lg"
+          isLoading={formState.isSubmitting}
         >
             Entrar
         </Button>
