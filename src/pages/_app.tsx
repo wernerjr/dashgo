8e7aee60c30afc604d10df4eb78039ade555bb47
Werner Gehrke Junior <werner.junior@outlook.com>
2021-07-09 16:02:16 -0300

    React query prefetch example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,15 +6,14 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 import { theme } from '../styles/theme'
 import { SidebarDrawerProvider } from '../context/SidebarDrawerContext'
 import { makeServer } from '../services/mirage'
+import { queryClient } from '../services/queryClient'
 
 if (process.env.NODE_ENV === 'development'){
   makeServer();
 }
 
-const queryClient = new QueryClient(); 
-
 function MyApp({ Component, pageProps }: AppProps) {
-  return (<QueryClientProvider client={queryClient}>
+  return (<QueryClientProvider client={ queryClient }>
             <ChakraProvider theme={theme}>
               <SidebarDrawerProvider>
                 <Component {...pageProps} />
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,12 +1,31 @@
-import { Box, Button, Flex, Heading, Icon, Table, Td, Th, Thead, Tr, Checkbox, Tbody, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
-import Link from "next/link";
+import { 
+  Box, 
+  Button, 
+  Flex, 
+  Heading, 
+  Icon, 
+  Table, 
+  Td, 
+  Th, 
+  Thead, 
+  Tr, 
+  Checkbox, 
+  Tbody, 
+  Text, 
+  useBreakpointValue, 
+  Spinner, 
+  Link 
+} from "@chakra-ui/react";
+import NextLink from "next/link";
 import { useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import Header from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
+import { api } from "../../services/api";
 import { useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
 
 export default function UserList() {
   const [page, setPage] = useState(1);
@@ -16,6 +35,16 @@ export default function UserList() {
     base: false,
     lg: true
   });
+
+  async function handlePrefetchUser(userId: number){
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`users/${userId}`)
+
+      return response.data;
+    }, {
+      staleTime: 1000 * 60 * 10
+    })
+  }
 
   return (
     <Box>
@@ -29,7 +58,7 @@ export default function UserList() {
               Usuários
               {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/>} 
             </Heading>
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -39,7 +68,7 @@ export default function UserList() {
               >
                 Criar Novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
           
           {isLoading ? (
@@ -71,7 +100,9 @@ export default function UserList() {
                     </Td>
                     <Td>
                       <Box>
-                        <Text fontWeight="bold" >{user.name}</Text>
+                        <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                          <Text fontWeight="bold" >{user.name}</Text>
+                        </Link>
                         <Text fontSize="sm" color="gray.300" >{user.email}</Text>
                       </Box>
                     </Td>
